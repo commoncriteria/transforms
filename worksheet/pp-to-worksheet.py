dddfe9576bded32d9b2c3238996b88b56436faa2
kgal <kevin@eclipse.ncsc.mil>
2018-07-18 09:38:01 -0400

    Mostly through removing minidom in favor of ElementTree.

--- worksheet/pp-to-worksheet.py
+++ worksheet/pp-to-worksheet.py
@@ -7,17 +7,26 @@ import base64
 from io import StringIO 
 import re
 import sys
-import xml.dom.minidom
-from xml.dom import minidom
+import xml.etree.ElementTree as ET
 from xml.sax.saxutils import escape
 PPNS='https://niap-ccevs.org/cc/v1'
 HTMNS="http://www.w3.org/1999/xhtml"
+ns={"cc":PPNS, "htm":HTMNS}
+
+def cc(tag):
+    return "{"+PPNS+"}"+tag
+
+def htm(tag):
+    return "{"+HTMNS + "}"+tag
 
 def warn(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-def getPpEls(parent, name):
-    return parent.getElementsByTagNameNS(PPNS, name)
+def attr(el,at):
+    if at in el.attrib: 
+        return el.attrib[at]
+    return ""
+
 
 class State:
     """Keeps track of certain values for a PP """
@@ -41,49 +50,59 @@ class State:
         # Holds the root
         self.root=theroot
         # Maps IDs to elements
-        self.idMap={}
-        self.harvest_ids(self.root)
+        self.parent_map = {c:p for p in self.root.iter() for c in p}
+        self.create_classmapping()
 
-    # getElementById doesn't work well with minidom,
-    # so we're faking.
-    def harvest_ids(self, node):
-        if node.nodeType == xml.dom.Node.ELEMENT_NODE:
-            self.idMap[node.getAttribute("id")]=node
-            for child in node.childNodes:
-                self.harvest_ids(child)
+    def up(self, node):
+        return self.parent_map[node]
+
+    def create_classmapping(self):
+        "Builds database to emulate getElementByClassname"
+        self.classmap={}
+        for el in self.root.findall(".//*[@class]"):
+            classes = el.attrib["class"].split(",")
+            for clazz in classes:
+                # If we already have this class in the classmap
+                if clazz in self.classmap:
+                    # Grab the old
+                    clazzset = self.classmap[clazz]
+                    clazzset.add(el)
+                else:
+                    self.classmap[clazz]={el}
+
 
     def handle_management_function_set(self, elem):
         ret = "<table class='mfun-table'>\n"
-        defaultVal = elem.getAttribute("default")
+        defaultVal = attr(elem,"default")
         if defaultVal == "":
             defaultVal="O"
             
         ret+= "<tr><th>Management Function</th>"
-        for col in getPpEls(elem, 'manager'):
+        for col in elem.findall( 'cc:manager', ns):
             ret += "<th>"
-            ret += self.handle_node(col, True)
+            ret += self.handle_contents(col, True)
             ret += "</th>"
         ret+= "</tr>\n"
 
         # Step through the rows
-        for row in getPpEls(elem, 'management-function'):
+        for row in elem.findall( 'cc:management-function', ns):
             val={}
             # Build a dictionary where the key is 'ref' and
             # it maps to 'M', 'O', or '-'.
-            for man in getPpEls(row, 'M'):
-                val[man.getAttribute('ref')]='M'
-            for opt in getPpEls(row, 'O'):
-                val[man.getAttribute('ref')]='O'
-            for das in getPpEls(row, '_'):
-                val[man.getAttribute('ref')]='-'
+            for man in row.findall( 'cc:M', ns):
+                val[man.attrib['ref']]='M'
+            for opt in row.findall( 'cc:O', ns):
+                val[man.attrib['ref']]='O'
+            for das in row.findall( 'cc:_', ns):
+                val[man.attrib['ref', ns]]='-'
             # Now we convert this to the expected columns
             ret += "<tr>\n"
             # First column is the management function text
-            ret += "<td>"+self.handle_collection( getPpEls(row, 'text')[0], True) + "</td>"
+            ret += "<td>"+self.handle_contents( row.find( 'cc:text', ns), True) + "</td>"
             # And step through every other column
-            for col in getPpEls(elem, 'manager'):
+            for col in elem.findall( 'cc:manager', ns):
                 ret += "<td>"
-                colId = col.getAttribute("id")
+                colId = col.attrib["id"]
                 if colId in val:
                     ret += self.man_fun_map[ val[colId] ]
                 else:
@@ -98,36 +117,35 @@ class State:
         sels=[]
         contentCtr=0
         ret="<span class='selectables"
-        if node.getAttribute("exclusive")=="yes":
+        if attr(node,"exclusive")=="yes":
             ret+=" onlyone"
         ret+="' data-rindex='"+ str(self.selectables_index) +"'>"
 
         self.selectables_index+=1
         rindex=0
-        for child in node.childNodes: # Hopefully only selectable
-            if child.nodeType == xml.dom.Node.ELEMENT_NODE and child.localName == "selectable":
-                contents = self.handle_node(child,True)
-                contentCtr+=len(contents)
-                chk = "<input type='checkbox'"
-                onChange=""
-                classes=""
-                if child.getAttribute("exclusive") == "yes":
-                    onChange+="chooseMe(this);"
-                id=child.getAttribute("id")
-                if id!="" and id in self.selMap:
-                    onChange+="updateDependency(this,"
-                    delim="["
-                    for sel in self.selMap[id]:
-                        classes+=" "+sel+"_m"
-                        onChange+=delim+"\""+sel+"\""
-                        delim=","
-                    onChange+="]);"
-                chk+= " onchange='update(); "+onChange+"'"
-                chk+= " data-rindex='"+str(rindex)+"'"
-                chk +=" class='val selbox"+classes+"'"
-                chk +="></input><span>"+ contents+"</span>\n"
-                sels.append(chk)
-                rindex+=1
+        for child in node.findall("cc:selectable", ns):
+            contents = self.handle_contents(child,True)
+            contentCtr+=len(contents)
+            chk = "<input type='checkbox'"
+            onChange=""
+            classes=""
+            if attr(child,"exclusive") == "yes":
+                onChange+="chooseMe(this);"
+            id=attr(child,"id")
+            if id!="" and id in self.selMap:
+                onChange+="updateDependency(this,"
+                delim="["
+                for sel in self.selMap[id]:
+                    classes+=" "+sel+"_m"
+                    onChange+=delim+"\""+sel+"\""
+                    delim=","
+                onChange+="]);"
+            chk+= " onchange='update(); "+onChange+"'"
+            chk+= " data-rindex='"+str(rindex)+"'"
+            chk +=" class='val selbox"+classes+"'"
+            chk +="></input><span>"+ contents+"</span>\n"
+            sels.append(chk)
+            rindex+=1
         # If the text is short, put it on one line
         if contentCtr < 50:
             for sel in sels:
@@ -141,71 +159,72 @@ class State:
         return ret+"</span>"
 
     def handle_cc_node(self, node, show_text):
-        if node.localName == "selectables":
+        warn("Handling "+ node.tag);
+        if node.tag == cc("selectables"):
             return self.handle_selectables(node)
 
-        elif node.localName == "refinement":
+        elif node.tag == cc("refinement"):
             ret = "<span class='refinement'>"
-            ret += self.handle_collection(node, True)
+            ret += self.handle_contents(node, True)
             ret += "</span>"
             return ret
 
-        elif node.localName == "assignable":
+        elif node.tag == cc("assignable"):
             ret = "<textarea onchange='update();' class='assignment val' rows='1' placeholder='"
-            ret += ' '.join(self.handle_collection(node, True).split())
+            ret += ' '.join(self.handle_contents(node, True).split())
             ret +="'></textarea>"
             return ret
 
-        elif node.localName == "abbr" or node.localName == "linkref":
+        elif node.tag == cc("abbr") or node.tag == cc("linkref"):
             if show_text:
-                return node.getAttribute("linkend")
+                return attr(node,"linkend")
 
-        elif node.localName == "management-function-set":
+        elif node.tag == cc("management-function-set"):
             ret=self.handle_management_function_set(node)
             return ret
 
-        elif node.localName == "section":
-            idAttr=node.getAttribute("id")
+        elif node.tag == cc("section"):
+            idAttr=node.attrib["id"]
             ret =""
             if "SFRs" == idAttr or "SARs" == idAttr:
-                ret+="<h2>"+node.getAttribute("title")+"</h2>\n"
-            ret += self.handle_collection(node, False)
+                ret+="<h2>"+node.attrib["title"]+"</h2>\n"
+            ret += self.handle_contents(node, False)
             return ret
 
-        elif node.localName == "ctr-ref" and show_text:
-            refid=node.getAttribute('refid')
+        elif node.tag == cc("ctr-ref") and show_text:
+            refid=node.attrib['refid']
             ret="<a onclick='showTarget(\"cc-"+refid+"\")' href=\"#cc-"+refid+"\" class=\"cc-"+refid+"-ref\">"
             target=self.idMap[refid]
-            prefix=target.getAttribute("ctr-type")+" "
+            prefix=target.attrib["ctr-type"]+" "
             if target.hasAttribute("pre"):
-                prefix=target.getAttribute("pre")
+                prefix=target.attrib["pre"]
             ret+="<span class=\"counter\">"+refid+"</span>"
             ret+="</a>"
             return ret
             
             
-        elif node.localName == "ctr" and show_text:
-            ctrtype=node.getAttribute("ctr-type")
+        elif node.tag == cc("ctr") and show_text:
+            ctrtype=node.attrib["ctr-type"]
             prefix=ctrtype+" "
             if node.hasAttribute("pre"):
-                prefix=node.getAttribute("pre")
-            idAttr=node.getAttribute("id")
+                prefix=node.attrib["pre"]
+            idAttr=node.attrib["id"]
             ret="<span class='ctr' data-myid='"+idAttr+"+data-counter-type='ct-"
             ret+=ctrtype+"' id='cc-"+idAttr+"'>\n"
             ret+=prefix
             ret+="<span class='counter'>"+idAttr+"</span>"
-            ret+=self.handle_collection(node, True)
+            ret+=self.handle_contents(node, True)
             ret+="</span>"
             return ret
         
-        elif node.localName == "f-element" or node.localName == "a-element":
-            # Requirements are handled in the title section
-            return self.handle_node( getPpEls(node, 'title')[0], True)
+        # elif node.tag == cc("f-element") or node.tag == cc("a-element"):
+        #     # Requirements are handled in the title section
+        #     return self.handle_contents( node.find( 'cc:title', ns), True)
 
-        elif node.localName == "f-component" or node.localName == "a-component":
-            status= node.getAttribute("status")
+        elif node.tag == cc("f-component") or node.tag == cc("a-component"):
+            status= attr(node,"status")
             ret=""
-            id=node.getAttribute("id")
+            id=node.attrib["id"]
             tooltip=""
             if status == "optional" or status == "objective":
                 ret+="<div class='tooltipped'>"
@@ -215,62 +234,69 @@ class State:
             ret+= "<span id='"+id+"'"
             # ret = "<div onfocusin='handleEnter(this)' id='"+id+"'"
             # The only direct descendants are possible should be the children
-            child=getPpEls(node, 'selection-depends')
+            node.findall( 'cc:selection-depends', ns)
             ret+=" class='component"
             if status!="":
                 ret+=" disabled"
             ret+="'>"
             #<a href='#"+id+"'>
-            ret+="<span class='f-comp-status'></span><a onclick='toggle(this); return false;' href='#"+id+"' class='f-comp-title'>"+id.upper()+" &mdash; "+ node.getAttribute("name")+"</a>"
+            ret+="<span class='f-comp-status'></span><a onclick='toggle(this); return false;' href='#"+id+"' class='f-comp-title'>"+id.upper()+" &mdash; "+ node.attrib["name"]+"</a>"
             ret+=tooltip
             ret+="\n<div class='reqgroup'>\n"
-            ret+=self.handle_collection(node, False)
+            ret+=self.handle_contents(node, False)
             ret+="\n</div></span></div><br></br>"
             return ret
-        elif node.localName == "title":
+        elif node.tag == cc("title"):
             self.selectables_index=0
-            req_id = node.parentNode.getAttribute('id')
-            com_id = node.parentNode.parentNode.getAttribute('id')
-            slaves = getPpEls(node.parentNode.parentNode, 'selection-depends')
+            req_id = self.up(node).attrib['id']
+            com_id = self.up(self.up(node)).attrib['id']
+            slaves = self.up(self.up(node)).findall( 'cc:selection-depends', ns)
             ret=""
             ret+="<div id='"+ req_id +"' class='requirement'>"
             ret+="<div class='f-el-title'>"+req_id.upper()+"</div>"
             ret+="<div class='words'>"
-            ret+=self.handle_collection(node, True)
+            ret+=self.handle_contents(node, True)
             ret+="</div>\n"
             ret+="</div>\n"
             return ret
         else:
-            return self.handle_collection(node, show_text)
+            warn("Skipped " + node.tag);
+            return self.handle_contents(node, show_text)
         return ""
 
-            
-    def handle_node(self, node, show_text):
-        """Converts singular XML nodes to text."""
-        if show_text and node.nodeType == xml.dom.Node.TEXT_NODE:
-            return escape(node.data)
-        elif node.nodeType == xml.dom.Node.ELEMENT_NODE:
-            if node.namespaceURI == PPNS:
-                return self.handle_cc_node(node, show_text)
-            elif node.namespaceURI == HTMNS:
-                if node.localName != "strike" and show_text:                     # Just ignore text that is striked out
-                    # Just remove the HTML prefix and recur.
-                    tag = re.sub(r'.*:', '', node.localName)
-                    ret = "<"+tag
-                    attrs = node.attributes
-                    for aa in range(0,attrs.length) :
-                        attr =attrs.item(aa)
-                        ret+=" " + attr.name + "='" + escape(attr.value) +"'"
-                    ret += ">"
-                    ret += self.handle_collection(node, True)
-                    ret += "</"+tag+">"
-                    return ret
-        return ""
 
-    def handle_collection(self, node, show_text):
+    def handle_node(self, node, show_text):           
+        if node.tag.startswith(cc("")):
+            return self.handle_cc_node(node, show_text)
+        elif node.tag.startswith(htm("")):
+            if not show_text or node.tag!= "strike":                     # Just ignore text that is striked out
+                return ""
+            # Just remove the HTML prefix and recur.
+            tag = re.sub(r'.*:', '', node.tag)
+            ret = "<"+tag
+            attrs = node.attributes
+            for aa in range(0,attrs.length) :
+                attr =attrs.item(aa)
+                ret+=" " + attr.name + "='" + escape(attr.value) +"'"
+            ret += ">"
+            ret += self.handle_contents(node, True)
+            ret += "</"+tag+">"
+            return ret
+        else:
+            warn("Just dropped something")
+
+    def handle_contents(self, node, show_text):
         ret=""
-        for child in node.childNodes:
-            ret += self.handle_node(child, show_text)
+        if show_text:
+            if node.text:
+                ret = escape(node.text)
+            for child in node:
+                ret+=self.handle_node(child, show_text)
+                if child.tail:
+                    ret+=child.tail
+        else:
+            for child in node:
+                ret+=self.handle_node(child, show_text)
         return ret
 
     def makeSelectionMap(self):
@@ -278,10 +304,10 @@ class State:
         Makes a dictionary that maps the master requirement ID
         to an array of slave component IDs
         """
-        for element in getPpEls(self.root, 'selection-depends'):
-            # req=element.getAttribute("req")
-            selIds=element.getAttribute("ids")
-            slaveId=element.parentNode.getAttribute("id")
+        for element in self.root.findall( 'cc:selection-depends', ns):
+            # req=element.attrib["req"]
+            selIds=element.attrib["ids"]
+            slaveId=self.up(element).attrib["id"]
             for selId in selIds.split(','):
                 reqs=[]
                 if selId in self.selMap:
@@ -309,7 +335,8 @@ if __name__ == "__main__":
         outfile=out[1]
 
     # Parse the PP and make state from it
-    state=State(minidom.parse(infile).documentElement)
+
+    state=State(ET.parse(infile).getroot())
 
     state.makeSelectionMap()
 
@@ -332,7 +359,7 @@ if __name__ == "__main__":
     form =  "<html xmlns='http://www.w3.org/1999/xhtml'>\n"
     form += "   <head>\n"
     form += "      <meta charset='utf-8'></meta>\n"
-    form += "      <title>"+state.root.getAttribute("name")+"</title>\n"
+    form += "      <title>"+state.root.attrib["name"]+"</title>\n"
     form += "      <style type='text/css'>\n"
     form += css
     form += """      </style>
@@ -347,7 +374,7 @@ if __name__ == "__main__":
        </head>       <body onkeypress='handleKey(event); return true;' onload='init();'>
 """
 
-    form +=  "      <h1>Worksheet for the " + state.root.getAttribute("name") + "</h1>\n"
+    form +=  "      <h1>Worksheet for the " + state.root.attrib["name"] + "</h1>\n"
     form +=  """
 <div class='warning-pane'>
    <noscript><h2 class="warning">This page requires JavaScript.</h2></noscript>
@@ -357,7 +384,7 @@ if __name__ == "__main__":
 </div>
     """
 
-    form += state.handle_node(state.root, False)
+    form += state.handle_contents(state.root, False)
     form += """
           <br/>
           <button type="button" onclick="generateReport()">XML Record</button>
