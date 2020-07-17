b269e32920b252968b49f04f60d559af9ad62790
kgal <kgal@users.noreply.github.com>
2020-07-16 21:20:31 -0400

    Fixed indent error in post-process.py

--- post-process.py
+++ post-process.py
@@ -30,7 +30,7 @@ def log(level, msg):
 
 
 def get_appendix_prefix(num):
-""" Gets the appendix prefix. 0 is A. """
+    """ Gets the appendix prefix. 0 is A. """
     if num >= 26:
         err("Cannot handle more than 26 appendices")
     ABC=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
