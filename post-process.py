8594418f04ff3777d8b4272d20a0766c4f50a915
Kevin G <kjgalli@radium.ncsc.mil>
2021-01-22 16:28:36 +0000

    Only fixed stylizer warnings in post-process.py. No functionality change (hopefully_).

--- post-process.py
+++ post-process.py
@@ -7,6 +7,7 @@ with XSLT).
 # from io import StringIO
 import re
 import sys
+import string
 import xml.etree.ElementTree as ET
 from xml.sax.saxutils import quoteattr, escape
 
@@ -33,8 +34,7 @@ def get_appendix_prefix(num):
     """ Gets the appendix prefix. 0 is A. """
     if num >= 26:
         err("Cannot handle more than 26 appendices")
-    ABC=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    return ABC[num]
+    return string.ascii_uppercase[num]
 
 
 def backslashify(phrase):
@@ -49,11 +49,11 @@ class State:
         self.period_ctr = 0
         self.abbrs = []
         self.key_terms = []
-        self.plural_to_abbr ={}
-        self.regex=None
+        self.plural_to_abbr = {}
+        self.regex = None
 
     def create_classmapping(self):
-        self.classmap={}
+        self.classmap = {}
         for el in self.root.findall(".//*[@class]"):
             classes = el.attrib["class"].split(",")
             # Go through all the classes the elment is a part of
@@ -64,11 +64,12 @@ class State:
                     clazzset = self.classmap[clazz]
                     # We're working with a list here, not a set
                     # Should really only not meet this if the
-                    # input document has an element where a class is listed twice
-                    if not el in clazzset:
+                    # input document has an element where a
+                    # class is listed twice
+                    if el not in clazzset:
                         clazzset.append(el)
                 else:
-                    self.classmap[clazz]=[el]
+                    self.classmap[clazz] = [el]
 
     def getElementsByClass(self, clazz):
         if clazz in self.classmap:
@@ -77,16 +78,16 @@ class State:
             return []
 
     def cross_reference_cc_items(self):
-        for clazz in {"assumption","threat","OSP","SOE", "SO"}:
-            for el in self.getElementsByClass(clazz): 
+        for clazz in {"assumption", "threat", "OSP", "SOE", "SO"}:
+            for el in self.getElementsByClass(clazz):
                 if "id" in el.attrib:
                     self.add_to_regex(el.attrib["id"])
 
     def build_comp_regex(self):
-        comps = self.getElementsByClass('reqid') + self.getElementsByClass('comp')
+        comps = self.getElementsByClass('reqid') +\
+            self.getElementsByClass('comp')
         for comp in comps:
             if 'id' in comp.attrib:
-#                 print("Adding " + comp.attrib["id"])
                 self.add_to_regex(comp.attrib["id"])
             else:
                 # The MDF has some unorthodox items
@@ -96,7 +97,7 @@ class State:
         for term in self.getElementsByClass('term'):
             if 'data-plural' in term.attrib:
                 plural = term.attrib['data-plural']
-                self.plural_to_abbr[plural]=term.text
+                self.plural_to_abbr[plural] = term.text
                 self.add_to_regex(plural)
             self.add_to_regex(term.text)
             self.abbrs.append(term.text)
@@ -104,17 +105,19 @@ class State:
     def add_to_regex(self, word):
         if len(word) > 1 and not(word.startswith(".")):
             self.key_terms.append(backslashify(word))
-            
+
     def to_html(self):
         try:
-            # Sort them so longer terms are searched first so 
+            # Sort them so longer terms are searched first so
             # that shorter terms embedded in larger terms don't
             # interfere with longer terms being found.
             self.key_terms.sort(key=len, reverse=True)
-            self.regex=re.compile("(?<!-)\\b(" + "|".join(self.key_terms) +")\\b")
+            regex_str = "(?<!-)\\b(" + "|".join(self.key_terms) + ")\\b"
+            self.regex = re.compile(regex_str)
         except re.error:
-            warn("Failed to compile regular expression: " + regex_str[:-1]+")\\b")            
-        self.ancestors=[]
+            warn("Failed to compile regular expression: " +
+                 regex_str[:-1]+")\\b")
+        self.ancestors = []
         return """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html>
 """ + self.to_html_helper(self.root)
@@ -127,14 +130,7 @@ class State:
             "h3"   in self.ancestors or "h4"      in self.ancestors or\
             "head" in self.ancestors or "script"  in self.ancestors
 
-
     def handle_text(self, parent, text):
-        # if parent.tag!="p" and parent.tag!="div" and parent.tag!="span":
-#        if parent.tag=="a"\
-#           or parent.tag=="{http://www.w3.org/1999/xhtml}a"\
-#           or parent.tag=="script"\
-#           or parent.tag=="style":
-         # No tags in tags.
         etext = escape(text)
         if self.is_in_non_xrefable_section():
             return etext
@@ -143,86 +139,89 @@ class State:
     def discover_xref(self, etext):
         if self.regex is None:
             return etext
-        last=0
-        ret=""
+        last = 0
+        ret = ""
         for mat in self.regex.finditer(etext):
-            ret += etext[last:mat.start()]                   # Append the characters between the last find and this
-            last = mat.end()                                 # Move the last indexer up
-            target = mat.group() 
+            # Append the characters between the last find and this
+            ret += etext[last:mat.start()]
+            # Move the last indexer up
+            last = mat.end()
+            target = mat.group()
             if mat.group() in self.plural_to_abbr:
                 target = self.plural_to_abbr[mat.group()]
-            if target in self.abbrs:                         # If target maches  an abbreviation
-                ret+='<abbr class="dyn-abbr"><a href="#abbr_'+target+'">'+mat.group()+'</a></abbr>'
+            # If target maches  an abbreviation
+            if target in self.abbrs:
+                ret += '<abbr class="dyn-abbr"><a href="#abbr_' + target+'">'
+                ret += mat.group()+'</a></abbr>'
             else:
-                ret+='<a href="#'+mat.group()+'">'+mat.group()+'</a>'
-        ret+=etext[last:]
+                ret += '<a href="#'+mat.group()+'">'+mat.group()+'</a>'
+        ret += etext[last:]
         return ret
-
 
     def to_html_helper(self, elem):
         """Function that turns document in HTML"""
         tagr = elem.tag.split('}')
-        noname=tagr[len(tagr)-1]
+        noname = tagr[len(tagr)-1]
         # Breaks elements are converted to empty tags
-        if noname=="br":
+        if noname == "br":
             return "<br/>"
-        if "class" in elem.attrib and elem.attrib["class"]=='no-link':
+        if "class" in elem.attrib and elem.attrib["class"] == 'no-link':
             self.ancestors.append("no-link")
 #        if noname=="span" and len(elem)==0 and elem.text is None:
 #            return "JJJ"
         else:
             self.ancestors.append(noname)
         # Everything else is beginning and end tags (even if they're empty)
-        ret="<" + noname
+        ret = "<" + noname
         for attrname in elem.attrib:
-            ret = ret + " " + attrname + "="+ quoteattr(elem.attrib[attrname])
-        ret=ret+">"
+            ret = ret + " " + attrname + "=" + quoteattr(elem.attrib[attrname])
+        ret = ret+">"
         if elem.text:
             ret += self.handle_text(elem, elem.text)
         for child in elem:
             ret += self.to_html_helper(child)
             if child.tail:
                 ret += self.handle_text(elem, child.tail)
-        ret= ret + '</' + noname +'>'
+        ret = ret + '</' + noname + '>'
         self.ancestors.pop()
         return ret
 
     def fix_counters(self):
         # Bail if there are no ctrs
-        occurs={}
+        occurs = {}
         # Go through all the counters
         for countable in self.getElementsByClass('ctr'):
             # Get the type of counter
             typee = countable.attrib['data-counter-type']
             # If we haven't seen it yet
-            if not typee in occurs:
+            if typee not in occurs:
                 # Make a list
-                occurs[typee]=0
+                occurs[typee] = 0
             # Increment by one
-            occurs[typee]+=1
+            occurs[typee] += 1
             countable.find("*[@class='counter']").text = str(occurs[typee])
 
-            refclass=countable.attrib["data-myid"]+"-ref"
+            refclass = countable.attrib["data-myid"]+"-ref"
             if refclass in self.classmap:
-                refs=self.classmap[refclass]
+                refs = self.classmap[refclass]
                 for ref in refs:
-                    ref.find("*[@class='counter']").text= str(occurs[typee])
+                    ref.find("*[@class='counter']").text = str(occurs[typee])
 
     def fix_tooltips(self):
         for elem in self.getElementsByClass("tooltiptext"):
-            attribs=self.parent_map[elem].attrib
+            attribs = self.parent_map[elem].attrib
             if "class" in attribs:
-                attribs["class"]=attribs["class"]+",tooltipped"
+                attribs["class"] = attribs["class"]+",tooltipped"
             else:
-                attribs["class"]="tooltipped"
+                attribs["class"] = "tooltipped"
 
     def fix_index_refs(self):
         for brokeRef in self.getElementsByClass("dynref"):
-            linkend=brokeRef.attrib["href"][1:]
-            target=root.find(".//*[@id='"+linkend+"']")
+            linkend = brokeRef.attrib["href"][1:]
+            target = root.find(".//*[@id='"+linkend+"']")
             if not hasattr(brokeRef, 'text')\
-               or brokeRef.text==None:
-                brokeRef.text=" "
+               or brokeRef.text == None:
+                brokeRef.text = " "
             try:
                 # Append ref text.
                 brokeRef.text = brokeRef.text + target.text
@@ -231,53 +230,49 @@ class State:
 
     def fix_indices(self):
         # Find the table of contents
-        toc=self.root.find(".//*[@id='toc']")
+        toc = self.root.find(".//*[@id='toc']")
         # Initialize the index number generator
-        inums=[0,0,0,0,0,0]
+        inums = [0, 0, 0, 0, 0, 0]
         # Initialize the is_alpha switch
-        is_alpha=False
+        is_alpha = False
         # Gather all elements with a data-level
-        eles=self.root.findall(".//*[@data-level]")
+        eles = self.root.findall(".//*[@data-level]")
         #
-        base=-1
+        base = -1
         # Go through the elemeents
-        for aa in range( len(eles)):
-            level=eles[aa].attrib["data-level"]
+        for aa in range(len(eles)):
+            level = eles[aa].attrib["data-level"]
 
             # If this is the first time we see an appendix
             if level == 'A' and not is_alpha:
-                inums=[-1,0,0,0,0]
-                is_alpha=True
+                inums = [-1, 0, 0, 0, 0]
+                is_alpha = True
 
             # Turn it into an index
             if level == 'A':
                 level = 0
             else:
-                level=int(level)
-                if base==-1:
-                    base=level
-                level=level-base
-
-
+                level = int(level)
+                if base == -1:
+                    base = level
+                level = level-base
             # If we have to pad out
             while level > len(inums):
                 inums.append(0)
             # If we go up one set
             if level+1 < len(inums):
-                inums[level+1]=0
-
-            inums[level]+=1
-
+                inums[level+1] = 0
+            inums[level] += 1
             if is_alpha and level == 0:
-                prefix= "Appendix " + get_appendix_prefix(inums[0]) + " - "
+                prefix = "Appendix " + get_appendix_prefix(inums[0]) + " - "
             elif is_alpha:
                 prefix = get_appendix_prefix(inums[0])
             else:
                 prefix = str(inums[0])
-            spacer=""
+            spacer = ""
             for bb in range(1, level+1):
                 prefix = prefix + "." + str(inums[bb])
-                spacer=spacer+"&nbps;"
+                spacer = spacer + "&nbps;"
 
             # Fix inline index number
             spany = ET.Element("span")
@@ -291,22 +286,22 @@ class State:
             # Why would an ID have to be escaped?
             # entry.attrib['href'] = '#'+escape(eles[aa].attrib['id'])
             entry.attrib['href'] = '#'+eles[aa].attrib['id']
-            entry.attrib['style']= 'text-indent:'+str(level*10)+ 'px'
+            entry.attrib['style'] = 'text-indent:'+str(level*10) + 'px'
 
-            entry.text=prefix
+            entry.text = prefix
             entry.append(spany)
             toc.append(entry)
-
 
     def handle_element(self, elem):
         pass
 
+
 def getalltext(elem):
-    ret=""
+    ret = ""
     if elem.text:
-        ret=elem.text
+        ret = elem.text
     for child in elem:
-        ret=ret+getalltext(child)+child.tail
+        ret = ret+getalltext(child)+child.tail
 
 
 if __name__ == "__main__":
@@ -314,22 +309,19 @@ if __name__ == "__main__":
         #        0                        1
         print("Usage: <protection-profile>[=<output-file>]")
         sys.exit(0)
-
     # Split on equals
-    out=sys.argv[1].split("=")
-    infile=out[0]
-    outfile=""
+    out = sys.argv[1].split("=")
+    infile = out[0]
+    outfile = ""
     if len(out) < 2:
-        outfile=infile.split('.')[0]+".html"
+        outfile = infile.split('.')[0]+".html"
     else:
-        outfile=out[1]
+        outfile = out[1]
 
-    if infile=="-":
-        root=ET.fromstring(sys.stdin.read())
+    if infile == "-":
+        root = ET.fromstring(sys.stdin.read())
     else:
-        root=ET.parse(infile).getroot()
-
-
+        root = ET.parse(infile).getroot()
     state = State(root)
     state.fix_indices()
     state.fix_index_refs()
@@ -345,4 +337,3 @@ if __name__ == "__main__":
     else:
         with open(outfile, "w+") as outstream:
             outstream.write(state.to_html().encode('utf-8'))
-
