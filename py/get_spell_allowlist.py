03d3f7de10a52e8f74420b4bcd7f04e32a68d5bc
Kevin G <kjgalli@radium.ncsc.mil>
2022-02-01 10:23:42 -0500

    Augmented get_spell_allowlist.py to grab CC-IDs of f-components and add the chunks to the spelling allowlist. Removed some common f-component IDs from the CommonCriteria spelling allowlist.

--- py/get_spell_allowlist.py
+++ py/get_spell_allowlist.py
@@ -6,18 +6,31 @@ Extracts all the defined capitalizations (also incorrectly known as acronyms) fr
 import sys
 import xml.etree.ElementTree as ET
 import os
+import re
+
 
 def extract_from_doc(path):
+    allowlist=set()
     ns = {'cc': "https://niap-ccevs.org/cc/v1",
       'sec': "https://niap-ccevs.org/cc/v1/section",
       'htm': "http://www.w3.org/1999/xhtml"}
     root = ET.parse(path).getroot()
     for term in root.findall(".//cc:term", ns):
-        if "abbr" in term.attrib:
-            print(term.attrib["abbr"])
-        if "plural" in term.attrib:
-            print(term.attrib["plural"])
+        add_val_if_attrib(term, "abbr", allowlist)
+        add_val_if_attrib(term, "plural", allowlist)
+    for comp in root.findall(".//cc:f-component", ns)+root.findall(".//cc:a-component", ns):
+        if "cc-id" in comp.attrib:
+            for part in re.split('[\._0-9]', comp.attrib["cc-id"]):
+                allowlist.add(part.upper())
+        add_val_if_attrib(comp, "iteration", allowlist)
+    for wurd in allowlist:
+        print(wurd)
+                
 
+def add_val_if_attrib(el, name, allowlist):
+    if name in el.attrib:
+        allowlist.add(el.attrib[name])
+            
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: <protection-profile>")
