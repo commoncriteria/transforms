129ecf1103638735ced8c1dc48e2c57125c1205e
kevin <kjgalli@radium.ncsc.mil>
2022-08-31 10:45:36 -0400

    Actually made it replace nodes and not just replacing a node's kids.

--- py/cc_apply_tds.py
+++ py/cc_apply_tds.py
@@ -13,6 +13,9 @@ if __name__ == "__main__":
         sys.exit(0)
     doc = ET.parse(sys.argv[1])
     root = doc.getroot()
+    parent_map = {c: p for p in root.iter() for c in p}
+
+
     # Need to sort
     for aa in sys.argv[2:]:
         td = ET.parse(aa).getroot()
@@ -21,9 +24,19 @@ if __name__ == "__main__":
             for xpath_spec in replace.findall(".//cc:xpath-specified", ns):
                 xpath = "."+ xpath_spec.attrib["xpath"]
                 replaced = root.find(xpath)
-                if replaced is not None:
-                    for kid in list(replaced):
-                        replaced.remove(kid)
-                    for kid in xpath_spec:
-                        replaced.append(kid)
+                if replaced is None:
+                    continue
+                parent = parent_map[replaced]
+                if parent is None:
+                    continue
+                kid_cache = []
+                for kid in list(parent):
+                    if kid == replaced:
+                        for newkids in list(xpath_spec):
+                            kid_cache.append(newkids)
+                    else:
+                        kid_cache.append(kid)
+                    parent.remove(kid)
+                for kid in kid_cache:
+                    parent.append(kid)
     print(ET.tostring(root, encoding='unicode', method='xml'))
