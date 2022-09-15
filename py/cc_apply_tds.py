04140e5d1d3a479665e9e8296f141e23fd8d123d
kevin <kjgalli@radium.ncsc.mil>
2022-09-15 09:03:44 -0400

    Reverted cc_apply_tds to a version that worked.

--- py/cc_apply_tds.py
+++ py/cc_apply_tds.py
@@ -3,6 +3,10 @@ import sys
 import pathlib
 import xml.etree.ElementTree as ET
 
+default_ns = {'cc': "https://niap-ccevs.org/cc/v1",
+      'sec': "https://niap-ccevs.org/cc/v1/section",
+      'htm': "http://www.w3.org/1999/xhtml"}
+
 def get_num(fullpath):
     path = pathlib.Path(fullpath)
     return int( path.stem )
@@ -12,51 +16,29 @@ def log(msg):
     sys.stderr.write("\n")
 
 
-def parse_td(td_path):
-    nsmap = {}
-    for event, param in  ET.iterparse(td_path, events=['start-ns', "end"]):
-        if event == "end":
-            root = param
-        elif event == "start-ns":
-            prefix,url = param
-            nsmap[prefix]=url
-    return root, nsmap
-
-    # sys.exit(0)
-    # # namespaces = {node[0]: node[1] for _, node in
-    
-    # for event, param in  ET.iterparse(sys.argv[1], events=['start-ns', "end"]):
-    #     if event == "end":
-    #         root = param
-    #     elif event == "start-ns":
-    #         prefix,url = param
-    #         print("End Namespace: " + prefix + " url: " + url)
-    # sys.exit(0)
-    # # namespaces = {node[0]: node[1] for _, node in
-
-    
 if __name__ == "__main__":
     if len(sys.argv)==1:
         print("Usage: <input-file> [<td1> [<td2> ...]]")
         sys.exit(0)
-
     doc = ET.parse(sys.argv[1])
     root = doc.getroot()
     parent_map = {c: p for p in root.iter() for c in p}
-
-
     ET.register_namespace('',"https://niap-ccevs.org/cc/v1")
     ET.register_namespace('sec',"https://niap-ccevs.org/cc/v1/section")
     ET.register_namespace('h', "http://www.w3.org/1999/xhtml")
+
 
     # Need to sort
     tds = sys.argv[2:]
     tds.sort(key=get_num)
     for td_path in tds:
-        # td = ET.parse(aa).getroot()
-
-        td, ns = parse_td(td_path)
-        
+        log("Parsing: "+td_path)
+        td = ET.parse(td_path).getroot()
+        ns_el = td.find(".//cc:xpath_namespaces",default_ns)
+        if ns_el is None:
+            ns = default_ns
+        else:
+            ns =ns_el.attrib 
         replaces = td.findall(".//cc:replace", ns)
         for replace in replaces:
             for xpath_spec in replace.findall(".//cc:xpath-specified", ns):
